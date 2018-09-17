fa56249f7ba112d6f043798673a66384f9fb5ec4
“huang054 <huangjun054@163.com>
2018-09-17 14:16:10 +0800

    first

---
+++ src/main/java/com/actuator/config/ChatbotSend.java
@@ -0,0 +1,41 @@
+package com.actuator.config;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+public class ChatbotSend {
+
+    public static String WEBHOOK_TOKEN = "https://oapi.dingtalk.com/robot/send?access_token=e1fdab5c11b2b667736434ec62e669bd6d58795d01137554069eca181738dae0";
+
+    public static void send(String msg) throws Exception{
+
+        HttpClient httpclient = HttpClients.createDefault();
+
+        HttpPost httppost = new HttpPost(WEBHOOK_TOKEN);
+        httppost.addHeader("Content-Type", "application/json; charset=utf-8");
+
+        String textMsg = "{ \"msgtype\": \"text\", \"text\": {\"content\":\""+msg+"\"}, \r\n" + 
+        		"    \"at\": {\r\n" + 
+        		"        \"atMobiles\": [\r\n" + 
+        		"            \"15813743541\", \r\n" + 
+        		//"            \"15279193694\", \r\n" + 
+        		"        ], \r\n" + 
+        		"        \"isAtAll\":false\r\n" + 
+        		"    }}";
+      
+        StringEntity se = new StringEntity(textMsg, "utf-8");
+        httppost.setEntity(se);
+
+        HttpResponse response = httpclient.execute(httppost);
+        if (response.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+            String result= EntityUtils.toString(response.getEntity(), "utf-8");
+            System.out.println(result);
+        }
+    }
+}
